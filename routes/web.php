8f76dae34dc9b95921d21b8fb798be1f621f8b8a
dannielnotfound <danielriber548@gmail.com>
2023-06-11 19:20:29 -0300

    fix: Breeze Views

--- routes/web.php
+++ routes/web.php
@@ -14,13 +14,6 @@ use App\Http\Controllers\EventController;
 */
 
 Route::get('/events', [EventController::class, 'index'])->name('events.index');
-Route::get('/events/create', [EventController::class, 'create'])->name('events.create');
-Route::get('/events/{id}', [EventController::class, 'show'])->name('events.show');
-Route::post('/store', [EventController::class, 'store'])->name('events.store');
-Route::get('/events/edit/{id}', [EventController::class, 'edit'])->name('events.edit');
-Route::put('/edit/{id}', [EventController::class, 'update'])->name('events.update');
-Route::get('/events/delete/{id}', [EventController::class, 'delete'])->name('events.delete');
-Route::delete('/delete/{id}', [EventController::class, 'destroy'])->name('events.destroy');
 
 Route::get('/', function () {
     return view('welcome');
@@ -34,4 +27,14 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    
+    Route::get('/events/create', [EventController::class, 'create'])->name('events.create');
+    Route::get('/events/{id}', [EventController::class, 'show'])->name('events.show');
+    Route::post('/store', [EventController::class, 'store'])->name('events.store');
+    Route::get('/events/edit/{id}', [EventController::class, 'edit'])->name('events.edit');
+    Route::put('/edit/{id}', [EventController::class, 'update'])->name('events.update');
+    Route::get('/events/delete/{id}', [EventController::class, 'delete'])->name('events.delete');
+    Route::delete('/delete/{id}', [EventController::class, 'destroy'])->name('events.destroy');
 });
+
+require __DIR__.'/auth.php';
