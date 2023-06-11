4afa3c2bd12c6855a66e3cc6923974bc92d5576b
dannielnotfound <danielriber548@gmail.com>
2023-06-11 19:10:17 -0300

    First Breeze Install

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,17 @@ Route::get('/events/edit/{id}', [EventController::class, 'edit'])->name('events.
 Route::put('/edit/{id}', [EventController::class, 'update'])->name('events.update');
 Route::get('/events/delete/{id}', [EventController::class, 'delete'])->name('events.delete');
 Route::delete('/delete/{id}', [EventController::class, 'destroy'])->name('events.destroy');
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
